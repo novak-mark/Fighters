54b8a8cc7a9844faf72a6e8c57a49d534df9e4cb
MarkNovak <novakovmark21@gmail.com>
2025-02-07 12:32:54 +0100

    fixed healthbars

--- src/logic.js
+++ src/logic.js
@@ -164,6 +164,8 @@ function initCanvas(){
     //display the timer again before starting it.
     document.getElementById("timer").style.display = "flex";
     document.getElementById("P1healthbar").style.display ="inline";
+    document.getElementById("P1bar").style.display ="inline";
+    document.getElementById("P2bar").style.display ="inline";
     document.getElementById("P2healthbar").style.display = "flex";
     document.getElementById("P1Icon").style.display = "flex";
     document.getElementById("P2Icon").style.display = "flex";
